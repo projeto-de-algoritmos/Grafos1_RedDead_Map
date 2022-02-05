334a057d1350e1c8381469a4f73c41c5581c13ab
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-02-05 02:26:02 -0300

    feat(models): implement graph functions

--- api/models.py
+++ api/models.py
@@ -1,17 +1,34 @@
 class GraphModel():
+    
+    def __init__(self, edges) -> None: 
+        """
+        Init graph as a dictionary
 
-    def __init__(self, edges) -> None:
-        self.neighbors = dict
+        edges = { "A": ["B", "C"], "B": ["A"] }
+        """
+        self.graph = dict
         self.add_edges(edges)
     
     def add_egdes(self, edges) -> None:
-        pass
+        """
+        Add edges to graph as dictionary of lists
+        """
+        self.graph = edges
 
     def get_edges(self) -> None:
-        pass
+        """
+        Returns all graph's edges
+        """
+        edges = []
+        for key in self.graph.keys():
+          edges.append(key)
+        return edges
 
     def get_nodes(self) -> list:
-        pass
+        """
+        Returns all graph's nodes
+        """
+        return list(self.graph.keys())
 
     def get_neighbors(self) -> dict:
-        return self.neighbors
+        return self.graph
