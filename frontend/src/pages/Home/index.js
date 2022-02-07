a6e1c0711eea7a81eb359a31f5d5f781afe07dcc
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-02-06 23:23:13 -0300

    feat(select): add select for cities
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import logo from '../../assets/logo.png';
+import api from '../../services/api';
 
-import { Navbar } from '../../components/Navbar';
+import { colors } from '../../theme/colors';
+
+import Select from 'react-select';
+import { Button } from '../../components/Button';
+
+import { SelectSection } from './styles';
 
 const Home = () => {
+
+  const [cities, setCities] = useState([]);
+
+  //load cities from api
+  useEffect(()=> {
+    const loadCities = async () => {
+      const { data } = await api.get('/cities');
+
+      const options = [];
+
+      data.forEach((e)=>{
+        options.push(
+          {
+            value: e,
+            label: e
+          }
+        );
+
+        return options;
+      })
+      
+      setCities(options);
+    }
+
+    loadCities();
+  }, [])
+
+  const selectStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      background: state.isSelected && colors.red,
+      color: state.isSelected ? colors.white : colors.black,
+      padding: 15,
+    })
+  }
+
   return (
     <div>
-      <Navbar imageSrc={logo} />
+      <SelectSection>
+        <Select 
+          options={cities}
+          placeholder='Escolha uma cidade inicial...'
+          styles={selectStyles}
+        />
+        <Select 
+          options={cities}
+          placeholder='Escolha uma cidade de destino...'
+          styles={selectStyles}
+        />
+        <Button text='Localizar' />
+      </SelectSection>
     </div>
   )
 };
