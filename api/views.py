7b07769ceb3c04f121c65cd8815869ae2545377e
viniciussaturnino <viniciussaturnino78@gmail.com>
2022-02-07 11:14:34 -0300

    feat: refactor views and add exceptions

--- api/views.py
+++ api/views.py
@@ -51,17 +51,46 @@ class RedDeadCitiesView():
     @app.route('/', methods=['GET'])
     def get() -> dict:
         """get method"""
-        return json.dumps(cities), 200
+        cities_obj = json.dumps(cities)
+
+        if not cities_obj:
+            error_response = {"status": "fail", "message": "Have no cities."}
+            return json.dumps(error_response), 404
+
+        return cities_obj, 200
 
     @app.route('/path', methods=['POST'])
     def post() -> list:
         """returns shortest path between two cities"""
         body = request.get_json()
-        start = body.get("start")
-        end = body.get("end")
-        return json.dumps(graph.find_shortest_path(start=start, end=end))
+
+        error_response = {"status": "fail", "message": "Invalid payload."}
+        
+        if not body:
+            return json.dumps(error_response), 400
+
+        try:
+            start = body.get("start")
+            end = body.get("end")
+            best_way = graph.find_shortest_path(start=start, end=end)
+        except KeyError:
+            return json.dumps(error_response), 400
+        except Exception:
+            response = {
+                "status": "fail",
+                "message": "Could not find the best way to this mission."
+            }
+            return json.dumps(response), 400
+
+        return json.dumps(best_way), 200
 
     @app.route('/cities', methods=['GET'])
     def get_cities() -> dict:
         """returns all nodes"""
-        return json.dumps(graph.get_nodes()), 200
+        nodes = json.dumps(graph.get_nodes())
+
+        if not nodes:
+            error_response = {"status": "fail", "message": "Invalid payload."}
+            return json.dumps(error_response), 404
+
+        return nodes, 200
